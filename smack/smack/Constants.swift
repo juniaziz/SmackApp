1308fae87321158197a772131af1f1f6ccf2a976
Aziz Ur Rehman <azizurrehman@Azizs-MacBook-Pro.local>
2017-08-03 11:17:38 +0400

    end of Lesson 6

--- smack/smack/Constants.swift
+++ smack/smack/Constants.swift
@@ -8,11 +8,23 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
 
 
+let BASE_URL = "https://smackysmackapp.herokuapp.com/v1"
+let URL_REGISTER = "\(BASE_URL)/account/register"
+let URL_LOGIN = "\(BASE_URL)/account/login"
+let ADD_USER = "\(BASE_URL)/user/add"
 
 //Segues
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+
+//User Defautls
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
