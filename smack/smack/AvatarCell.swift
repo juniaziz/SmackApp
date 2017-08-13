1228d5cebb63a4a5ec45448fe126afaff189ac61
Aziz Ur Rehman <azizurrehman@Azizs-MacBook-Pro.local>
2017-08-13 16:18:01 +0400

    13 commit

---
+++ smack/smack/AvatarCell.swift
@@ -0,0 +1,13 @@
+//
+//  AvatarCell.swift
+//  smack
+//
+//  Created by Aziz Ur Rehman on 8/13/17.
+//  Copyright © 2017 JAZIZ. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+}
